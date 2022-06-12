51d8a42f3c7c66de5d82246897ce07b7d4b5d5bc
vince-exe <vincenzocaliendoantonio@gmail.com>
2022-06-12 02:23:03 +0200

    refactoring

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,33 @@
 import os
 
-from utils.enums import JsonReader, GeneralErrors
-
-from errors.errors import read_json_errors
-
+from utils.enums import JsonReader, GeneralErrors, Colors
 
 import json
+
+
+def read_json_errors(error):
+    if error == JsonReader.FileNotFound:
+        print(f"{Colors.Red}\nERROR: {Colors.Reset}Config file not found")
+        return False
+
+    elif error == JsonReader.DecodeError:
+        print(f"{Colors.Red}\nERROR: {Colors.Reset}Can't read the {JsonReader.FileName} file")
+        return False
+
+    elif error == JsonReader.KeyModified:
+        print(f"{Colors.Red}\nERROR: {Colors.Reset}Can't read the config file, it seems that you have modified a key"
+              f" in the config file")
+        return False
+
+    elif error == JsonReader.MaxPrefError:
+        print(f"{Colors.Red}\nERROR: {Colors.Reset}You overpassed the max len of the prefix")
+        return False
+
+    elif error == GeneralErrors.ValueError_:
+        print(f"{Colors.Red}\nERROR: {Colors.Reset}Check the config file")
+        return False
+
+    return error
 
 
 def read_json():
